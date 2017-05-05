3fbd7351bdbdfc88be6d778ffef379bd8f6e8b72
Basma1412 <13p6036@eng.asu.edu.eg>
2017-05-05 22:44:13 +0200

    ..

--- JavaApplication8/src/javaapplication8/JavaApplication8.java
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -1,16 +1,6 @@
 package javaapplication8;
 
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.Random;
 import javax.swing.*;
 
 class Board extends JFrame {
